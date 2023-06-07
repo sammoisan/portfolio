442024075302ae421d005f714236db59593d091c
Samuel Moisan <sam.moisan@gmail.com>
2023-06-07 17:08:53 -0400

    version 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { WorkComponent } from './work/work.component';
+import { ExpertiseComponent } from './expertise/expertise.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    WorkComponent,
+    ExpertiseComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
